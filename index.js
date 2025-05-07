0bacd1ca1e3ae1dc5d5f6a3cc7b1acbdbecef670
KumarSaurav12-code <78965358+KumarSaurav12-code@users.noreply.github.com>
2025-05-07 08:10:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ const no = document.querySelector("#no");
 const confirmation = document.querySelector(".confirmation");
 const container = document.querySelector(".container");
 const confirmationNo = document.querySelector(".confirmationNo");
+const confirmationNo1 = document.querySelector(".confirmationNo1");
 
 yesAnswerMethod = () => {
     container.style.display = 'none'
     confirmationNo.style.display = 'none'
+    confirmationNo1.style.display = 'none'
     confirmation.style.display = 'block'
 }
 
 noAnswerMethod = () => {
     container.style.display = 'none'
     confirmationNo.style.display = 'block'
+    confirmationNo1.style.display = 'block'
 }
 
 no.addEventListener('click', noAnswerMethod)
 yes.addEventListener('click', yesAnswerMethod)
 
 document.getElementById("fix").addEventListener("click", function () {
-    window.location = "https://wa.me/+919953215630?text=Yes I will think!!!!! ";
+    window.location = "https://wa.me/+919953215630?text=Yes!!!!!!!!! ";
 }, false);
 
 document.getElementById("nofix").addEventListener("click", function () {
-    window.location = "https://wa.me/+919953215630?text= Better luck next time!!!!! ";
+    window.location = "https://wa.me/+919953215630?text= No and Muh mat dikhana apna ";
 }, false);
